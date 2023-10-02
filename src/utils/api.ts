ee4bde4f7248851edacf96503cfc4c4fb3ccd6ef
Valerie <kekvalery@gmail.com>
2023-10-03 01:16:40 +0700

    feat: create api function getAsteroidsInfo

---
+++ src/utils/api.ts
@@ -0,0 +1,14 @@
+import { NASA_API_URL, API_KEY } from "./constants";
+import { DateRange } from "./types";
+
+export async function getAsteroidsInfo(dateRange: DateRange) {
+  const res = await fetch(
+    `${NASA_API_URL}/feed?start_date=${dateRange.startDate}&end_date=${dateRange.endDate}&api_key=${API_KEY}`
+  );
+
+  if (!res.ok) {
+    console.log("Failed to fetch data");
+  }
+
+  return res.json();
+}
