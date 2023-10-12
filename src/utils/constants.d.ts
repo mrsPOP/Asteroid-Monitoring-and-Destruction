08239452ba3c95ee793c3518220ad1dae1d07846
Valerie <kekvalery@gmail.com>
2023-10-12 22:34:55 +0700

    chore: add MONTHS constant

--- src/utils/constants.d.ts
+++
@@ -1,2 +0,0 @@
-const NASA_API_URL = 'https://api.nasa.gov/neo/rest/v1';
-const API_KEY = 'gFXoUemaCQrU1KtKItIgppcB3JFvtci0SSz9EbIJ';
---
+++ src/utils/constants.ts
@@ -0,0 +1,16 @@
+export const NASA_API_URL = 'https://api.nasa.gov/neo/rest/v1';
+export const API_KEY = 'gFXoUemaCQrU1KtKItIgppcB3JFvtci0SSz9EbIJ';
+export const MONTHS: { [key: string]: string } = {
+  '01': 'Янв',
+  '02': 'Фев',
+  '03': 'Мар',
+  '04': 'Апр',
+  '05': 'Май',
+  '06': 'Июн',
+  '07': 'Июл',
+  '08': 'Авг',
+  '09': 'Сен',
+  '10': 'Окт',
+  '11': 'Ноя',
+  '12': 'Дек',
+};
