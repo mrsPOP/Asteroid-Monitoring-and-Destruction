acb17ac540b77013d4f5140e3fba057e7613a997
Valerie <kekvalery@gmail.com>
2023-10-13 00:39:58 +0700

    feat: create DistanceUnit component

---
+++ app/components/DistanceUnit/DistanceUnit.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React from "react";
+import { useDistanceUnitStore } from "@/hooks/useStore";
+import styles from "./DistanceUnit.module.css";
+import classNames from "classnames";
+
+const DistanceUnit = () => {
+  const { inKilometers, switchInKilometers } = useDistanceUnitStore();
+
+  return (
+    <div className={styles["container"]}>
+      <button
+        className={classNames(
+          styles["general-for-button"],
+          { [styles["selected"]]: inKilometers },
+          { [styles["not-selected"]]: !inKilometers }
+        )}
+        onClick={switchInKilometers}
+      >
+        в километрах
+      </button>
+      <span>|</span>
+      <button
+        className={classNames(
+          styles["general-for-button"],
+          { [styles["selected"]]: !inKilometers },
+          { [styles["not-selected"]]: inKilometers }
+        )}
+        onClick={switchInKilometers}
+      >
+        в лунных орбитах
+      </button>
+    </div>
+  );
+};
+
+export default DistanceUnit;
