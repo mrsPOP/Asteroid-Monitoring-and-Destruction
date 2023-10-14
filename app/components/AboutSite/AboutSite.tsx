289f9a38590e5022871b20aeb2d6ebac42ee5aba
Valerie <kekvalery@gmail.com>
2023-10-15 01:32:55 +0700

    feat: create AboutSite component

---
+++ app/components/AboutSite/AboutSite.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./AboutSite.module.css";
+import { Passion_One } from 'next/font/google';
+import classNames from "classnames";
+
+const passionOne = Passion_One({ subsets: ['latin'], weight: "400" });
+
+const AboutSite = () => {
+  return (
+    <div className={styles["container"]}>
+      <p className={classNames(styles["logo"], passionOne.className)}>ARMAGEDDON 2023</p>
+      <p className={styles["about"]}>ООО “Команда им. Б. Уиллиса”. Взрываем астероиды с 1998 года.</p>
+    </div>
+  );
+};
+
+export default AboutSite;
