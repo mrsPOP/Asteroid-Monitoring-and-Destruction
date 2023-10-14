3b020e6ffc4be5a0d46b0a59842547ab64707886
Valerie <kekvalery@gmail.com>
2023-10-15 01:34:31 +0700

    feat: add normalize.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import "../public/styles/normalize.css";
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import AboutSite from "./components/AboutSite/AboutSite";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ru">
+      <body className={inter.className}>
+        <AboutSite />
+        {children}
+      </body>
     </html>
   )
 }
