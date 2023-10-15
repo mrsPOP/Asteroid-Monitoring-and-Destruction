354ab12f6e56fcb9da77ed675fcc71a5a06cbc1c
Valerie <kekvalery@gmail.com>
2023-10-15 23:33:37 +0700

    chore: finish Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
 import styles from "./page.module.css";
 import AsteroidsList from "@/components/AsteroidsList/AsteroidsList";
+import DistanceUnit from "@/components/DistanceUnit/DistanceUnit";
+import PageTitle from "./components/PageTitle/PageTitle";
+import Cart from "./components/Cart/Cart";
+import RenderedAsteroids from "./components/RenderedAsteroids/RenderedAsteroids";
+import { getAsteroidsList } from "@/utils/helperFunctions";
 
 export default async function Home() {
+  const data = await getAsteroidsList();
+
+  let firstStaticAsteroidsData: AsteroidInfo[];
+  let notRenderedAstroids: AsteroidInfo[] = [];
+
+  if (data.length > 8) {
+    firstStaticAsteroidsData = data.slice(0, 8);
+    notRenderedAstroids = data.slice(8).reverse();
+  } else {
+    firstStaticAsteroidsData = data;
+  }
+
   return (
     <main className={styles.main}>
-      <AsteroidsList />
+      <PageTitle />
+      <DistanceUnit />
+      <RenderedAsteroids firstStaticAsteroidsData={firstStaticAsteroidsData}/>
+      <AsteroidsList notRenderedAstroids={notRenderedAstroids}/>
+      <Cart />
     </main>
   );
 }
