52783e74643a12524596fcdfb4c0d0a2be39308a
Valerie <kekvalery@gmail.com>
2023-10-19 17:04:09 +0700

    refactor: use Link for logo, fix styles for AboutSite

--- src/components/AboutSite/AboutSite.tsx
+++ src/components/AboutSite/AboutSite.tsx
@@ -1,13 +1,14 @@
 import styles from "./AboutSite.module.css";
 import { Passion_One } from 'next/font/google';
 import classNames from "classnames";
+import Link from "next/link";
 
 const passionOne = Passion_One({ subsets: ['latin'], weight: "400" });
 
 const AboutSite = () => {
   return (
     <div className={styles["container"]}>
-      <p className={classNames(styles["logo"], passionOne.className)}>ARMAGEDDON 2023</p>
+      <Link href='/' className={classNames(styles["logo"], passionOne.className)}>ARMAGEDDON 2023</Link>
       <p className={styles["about"]}>ООО “Команда им. Б. Уиллиса”.<br/>Взрываем астероиды с 1998 года.</p>
     </div>
   );
