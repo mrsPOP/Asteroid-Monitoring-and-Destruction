3cfe0e2555350cadf17280d3d9637e4b14d09b8b
Valerie <kekvalery@gmail.com>
2023-10-20 12:42:58 +0700

    fix: disable button when cart is empty

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -4,16 +4,27 @@ import { useCartStore } from "@/hooks/useStore";
 import { asteroidDeclensions } from "@/utils/constants";
 import styles from "./Cart.module.css";
 import { useRouter } from "next/navigation";
+import classNames from "classnames";
 
 const Cart = () => {
-  const {cart} = useCartStore();
+  const { cart } = useCartStore();
   const router = useRouter();
 
   return (
     <div className={styles["cart-container"]}>
       <h2 className={styles["cart-title"]}>Корзина</h2>
-      <p className={styles["asteriods-number"]}>{`${cart.size} ${asteroidDeclensions[cart.size % 10]}`}</p>
-      <button className={styles["send-button"]} onClick={() => router.push('/order-status')}>Отправить</button>
+      <p className={styles["asteriods-number"]}>{`${cart.size} ${
+        asteroidDeclensions[cart.size % 10]
+      }`}</p>
+      <button
+        className={classNames(styles["send-button"], {
+          [styles["disabled-btn"]]: cart.size === 0,
+        })}
+        onClick={() => router.push("/order-status")}
+        type="button"
+      >
+        Отправить
+      </button>
     </div>
   );
 };
