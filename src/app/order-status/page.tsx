36bb6a1a97cd562b9dc98a0fab17569cf9f70206
Valerie <kekvalery@gmail.com>
2023-10-26 18:05:58 +0700

    fix: clear cart after order is done

--- src/app/order-status/page.tsx
+++ src/app/order-status/page.tsx
@@ -1,19 +1,26 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useCartStore } from "@/hooks/useStore";
 import AsteroidItem from "@/components/AsteroidItem/AsteroidItem";
 import styles from "./styles.module.css";
 
 const OrderPage = () => {
-  const cart = useCartStore((state) => state.cart) as Map<string, AsteroidInfo>;
+  const { cart, clearCart } = useCartStore();
+
+  const [cartCopy, _] = useState(Array.from(cart));
+
+  useEffect(() => () => clearCart(), []);
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Заказ отправлен!</h1>
+      <h1 className={styles.title}>Заказ отправлен!</h1>(
       <ul className={styles.list}>
-        {Array.from(cart).map(([key, value]: [string, AsteroidInfo]) => (
+        {cartCopy.map(([key, value]) => (
           <AsteroidItem key={key} data={value} />
         ))}
       </ul>
+      )
     </div>
   );
 };
